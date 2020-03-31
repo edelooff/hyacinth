14b75312dc1f54fa4d075980de0dd3b247bef4d6
Elmer de Looff <elmer.delooff@gmail.com>
2020-03-31 21:57:28 +0200

    Create and print bouquets from provided flowers and maintain inventory.

--- hyacinth.py
+++ hyacinth.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import random
 import re
 import sys
 
@@ -16,6 +17,7 @@ class BouquetDesigner:
     def __init__(self, design, size, required_flowers, total_count):
         self.design = design
         self.size = size
+        self.random_flower_count = total_count - sum(required_flowers.values())
         self.required_flowers = required_flowers
         self.total_count = total_count
         self.pool_available = Counter()
@@ -38,6 +40,46 @@ class BouquetDesigner:
                 return True
         return False
 
+    def create(self, pool):
+        """Returns a bouquet (species listing) assembled from the given pool.
+
+        After picking the required flowers, if additional flowers are needed,
+        this method selects a sample of flowers from the pool at random. This
+        avoids pathological behaviour where it *consistently* steals the same
+        flower speciess required by other designs, it simply picks a few of
+        everything, with a bias to flowers there are a lot of.
+        """
+        bouquet = Counter()
+        for species, count in self.required_flowers.items():
+            pool[species] -= count
+            bouquet[species] += count
+        # Pick the remaining flowers
+        if self.random_flower_count:
+            population = []
+            for species, count in pool.items():
+                population.extend([species] * count)
+            for species in random.sample(population, self.random_flower_count):
+                pool[species] -= 1
+                bouquet[species] += 1
+        return bouquet
+
+    def remove(self, species, size, quantity=1):
+        """Proceses removal of flowers from the flower pool.
+
+        This will update either the cache for available required flowers, or
+        if it's a speciess not -required- for this deisgn, the wildcard count.
+        """
+        if size == self.size:
+            if species in self.required_flowers:
+                self.pool_available[species] -= quantity
+            else:
+                self.wildcards -= quantity
+
+    def stringify_bouquet(self, bouquet):
+        flowers = sorted(bouquet.items())
+        flowerstring = (f'{count}{species}' for species, count in flowers)
+        return f'{self.design}{self.size}{"".join(flowerstring)}'
+
     @classmethod
     def from_specification(cls, design):
         spec = DESIGN.match(design).groupdict()
@@ -49,14 +91,19 @@ class BouquetDesigner:
 
 def main():
     designers = []
-    pool = Counter()
+    pools = {'L': Counter(), 'S': Counter()}
     for design in iter(sys.stdin.readline, '\n'):
         designers.append(BouquetDesigner.from_specification(design))
     for species, size in (line.strip() for line in sys.stdin):
-        pool[species + size] += 1
+        pools[size][species] += 1
         for designer in designers:
             if designer.added(species, size):
-                print(f'Designer {designer} can create a bouquet')
+                bouquet = designer.create(pools[size])
+                # Move this out of the main loop probably
+                for b_species, quantity in bouquet.items():
+                    for gen in designers:
+                        gen.remove(b_species, size, quantity=quantity)
+                print(designer.stringify_bouquet(bouquet))
 
 
 if __name__ == '__main__':
